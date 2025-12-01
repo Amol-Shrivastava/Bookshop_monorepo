1ce44e39831f89f9028d13a0fe55b4241798f665
amol <amolshrivastava448@gmail.com>
2025-12-01 20:35:47 +0530

    changed the imported csn file at bookstore level where it was outdated csn version

--- packages/bookstore/srv/mashup.js
+++ packages/bookstore/srv/mashup.js
@@ -3,13 +3,24 @@ const cds = require("@sap/cds");
 class BookstoreCatalogService extends cds.ApplicationService {
   async init() {
     const CatalogService = await cds.connect.to("CatalogService");
+    // const ReviewServiceAPI = await cds.connect.to("ReviewServiceAPI");
 
-    this.on("READ", "Books", async (req) => {
-      debugger;
-      return CatalogService.read(req.query);
-    });
+    // this.on("READ", "Books", async (req) => {
+    //   return CatalogService.read(req.query);
+    // });
 
-    await super.init();
+    CatalogService.prepend((srv) =>
+      srv.on("READ", "Books/reviews", (req) => {
+        console.debug("> delegating request to ReviewServiceAPI");
+        const { id } = req.params,
+          { columns, limit } = req.query.SELECT;
+        return ReviewService.read("Reviews", columns)
+          .limit(limit)
+          .where({ subject: String(id) });
+      })
+    );
+
+    return super.init();
   }
 }
 
